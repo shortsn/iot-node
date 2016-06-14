07c48a66e206b13829459aeb16c82e7809cc4d93
shortsn <shortsn@users.noreply.github.com>
2016-06-14 22:26:14 +0200

    fump

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ var app     = express();
 
 app.use(express.static(__dirname + '/Site'));
 
-/*
-var five = require("johnny-five");
-var board = new five.Board();
 
-board.on("ready", function() {
-  // Create an Led on pin 13
-  var led = new five.Led(13);
-  // Blink every half second
-  led.blink(50);
-});
-*/
+// var five = require("johnny-five");
+// var board = new five.Board();
+
+// board.on("ready", function() {
+//   // Create an Led on pin 13
+//   var led = new five.Led(13);
+//   // Blink every half second
+//   led.blink(50);
+// });
+
 
 var server = app.listen(8080, function() {
 
