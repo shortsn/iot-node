46652d522a3ef15ac2bc68148c3d9859edce651a
shortsn <shortsn@users.noreply.github.com>
2016-06-14 20:44:34 +0200

    fump

---
+++ app.js
@@ -0,0 +1,25 @@
+﻿var express = require('express');
+var app     = express();
+
+app.use(express.static(__dirname + '/Site'));
+
+/*
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  // Create an Led on pin 13
+  var led = new five.Led(13);
+  // Blink every half second
+  led.blink(50);
+});
+*/
+
+var server = app.listen(8080, function() {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Freeboard running at http://%s:%s', host, port);
+
+});
